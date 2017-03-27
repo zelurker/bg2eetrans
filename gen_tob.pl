c7d3875edd089262df7fe3d3973110c161f2873d
Emmanuel Anne <emmanuel.anne@gmail.com>
2017-03-27 21:28:11 +0200

    et amélioration de la partie tob
    en gros toutes les chaines après 74000 environ étaient passées à la
    trappe, donc avec un script je les ai ramenées sur le même modèle que
    pour bgee dans le commit d'avant. La différence c'est qu'on garde ce qui
    a déjà été fait pour tob, on ne fait que retoucher et ajouter, on ne
    repart pas de 0 ici !

---
+++ gen_tob.pl
@@ -0,0 +1,85 @@
+# tiré de gen_bgee mais pour tob, donc on ne adapte l'existant...
+
+use strict;
+use v5.20;
+
+my (%us,%tob);
+read_tra("us.tra",\%us);
+read_tra("tob_us.tra",\%tob);
+my %out;
+my ($min,$max) = (200000,0);
+foreach (keys %us) {
+	if ($tob{$_}) {
+		my $s = $_;
+		my $bg = 200000;
+		foreach (@{$tob{$s}}) { # on garde l'indice le + petit trouvé parce que certaines chaines ne sont pas traduites dans la vf de tob !
+			# malheureusement même comme ça on récupère quand même des
+			# chaines anglaises !
+			$bg = $_ if ($_ < $bg);
+		}
+		next if ($bg > 74106); # + de traducs sur l'us que sur le fr !!!
+		$min = $bg if ($bg < $min);
+		$max = $bg if ($bg > $max);
+		foreach (@{$us{$s}}) {
+			$out{$_} = $bg;
+		}
+	}
+}
+my @fr;
+open(F,"<bg2eetrans/tob.tph") || die "can't read tob.tph !\n";
+open(G,">tob2.tph") || die "can't create tob2.tph\n";
+say G "// tob.tph : automatically generated, exact matches only !";
+say G "// (ignoring ponctuation)";
+say G "";
+my ($key,$last,$val);
+foreach (sort { $a <=> $b } keys %out) {
+	my $num = $_;
+	if (!$key || $key < $_) {
+		while (<F>) {
+			if (/STRING_SET ~(\d+)~ @(\d+)/) {
+				$key = $1;
+				$last = $_;
+				$val = $2;
+				print G $last if ($key < $num);
+				last if ($key >= $num);
+			}
+		}
+	}
+	if ($key != $num || ($key == $num &&
+			($last =~ /^\/\// || $val != $out{$num}))) {
+		say "$num added/fixed";
+		say G "\tSTRING_SET ~$num~ \@$out{$num}\t// gen_tob.pl prev: $key / $val";
+	} elsif ($key == $num) {
+		print G $last;
+	}
+}
+close(G);
+close(F);
+
+sub read_tra {
+	my ($name,$hash) = @_;
+	say "reading tra $name";
+	open(G,"<$name") || die "read_tra: $name\n";
+	while (<G>) {
+		next if (/^\/\//);
+		/^\@(\d+) += (.)(.+)/;
+		my $pre = $2; # préfixe de la valeur à réutiliser
+		my $nb = 0;
+		while (!/($pre|\])$/) {
+			$_ .= <G>;
+		}
+		chomp;
+		$_ = lc($_);
+		s/[,;!\?\:\.]/ /g;
+		s/ +/ /g;
+		if (/^\@(\d+) += (.+)/s) {
+			if (!$$hash{$2}) {
+				$$hash{$2} = [$1];
+			} else {
+				push @{$$hash{$2}}, $1;
+			}
+		}
+	}
+	close(G);
+}
+
